601012c522ab90ecb9eb767aea5ad955d7ffa6e0
Luis Garay <55920809+LuisGarayF@users.noreply.github.com>
2022-04-07 22:15:34 -0400

    [FIX] Reparación de menú

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,18 +1,26 @@
 
+// Evento listener 
+
 $(document).ready(function () {
 
     $('[data-toggle="tooltip"]').tooltip()
 
+
+    // Al hacer clic en el boton se ejecuta un alert
     $("#enviarCorreo").click(function(){
         alert("El correo fue enviado correctamente...");
     });
 
+
+    // Cambio de color en los titulos h4 con doble clic
     $("h4").on("dblclick", function(){
         $(this).css({
         "color": "red",
         });
     });
 
+
+    // Al hacer un clic se activa el metodo toogle para ocultar el texto en p
     $(".title h5").click(function() {
         $(".title p").toggle();
     });
