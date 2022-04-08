cc62f98adf6c8e0bff50230c50bcb3ce87824380
Luis Garay <55920809+LuisGarayF@users.noreply.github.com>
2022-04-07 20:44:26 -0400

    [ADD] First commit HTML CSS

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,23 @@
 
+$(document).ready(function () {
 
+    $('[data-toggle="tooltip"]').tooltip()
 
+    $("#enviarCorreo").click(function(){
+        alert("El correo fue enviado correctamente...");
+    });
 
+    $("h4").on("dblclick", function(){
+        $(this).css({
+        "color": "red",
+        });
+    });
+
+    $(".title h5").click(function() {
+        $(".title p").toggle();
+    });
+
+})
 
 
 
